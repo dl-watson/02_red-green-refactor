ee6ff72024783e3f93f5f7edd7071f134fb615b5
dl-watson <dlarkinwatson@gmail.com>
2020-12-01 15:19:16 -0800

    testing successful fetch, not yet in the correct format

--- 4_fetchQuotes/fetchQuotes.js
+++ 4_fetchQuotes/fetchQuotes.js
@@ -1,2 +1,23 @@
-const fetchQuotes = () => {};
+const request = require("superagent");
+const _ = require("lodash");
+
+const fetchQuotes = async () => {
+  try {
+    const response = await request.get(
+      "http://futuramaapi.herokuapp.com/api/quotes/"
+    );
+
+    return response.body.slice(0, 1);
+
+    // return response.body.
+    //   .map(({ character: name }) => name)
+    //   .map(({ quote: text }) => text)
+    //   .map(({ image }) => image);
+  } catch (err) {
+    console.log("Error!: ", err);
+  }
+};
+
 module.exports = { fetchQuotes };
+
+fetchQuotes();
--- 4_fetchQuotes/fetchQuotes.test.js
+++ 4_fetchQuotes/fetchQuotes.test.js
@@ -1,18 +1,29 @@
 const { fetchQuotes } = require("./fetchQuotes");
+const request = require("superagent");
+
+jest.mock("superagent");
 
 describe("fetchQuotes", () => {
-  it("return as the result of an API fetch a single quote as an object with name, text and image properties", () => {
-    const output = {
-      name: "Bender",
-      text: "I'm a fraud. A poor, lazy, sexy fraud.",
-      image:
-        "https://res.cloudinary.com/dzxqhkyqd/image/fetch/c_scale,w_500/https://res.cloudinary.com/dzxqhkyqd/image/upload/v1552429540/bender.png",
-    };
-    expect(output).toEqual({
-      name: "Bender",
-      text: "I'm a fraud. A poor, lazy, sexy fraud.",
-      image:
-        "https://res.cloudinary.com/dzxqhkyqd/image/fetch/c_scale,w_500/https://res.cloudinary.com/dzxqhkyqd/image/upload/v1552429540/bender.png",
+  it("return as the result of an API fetch a single quote as an object with name, text and image properties", async () => {
+    request.get.mockResolvedValue({
+      body: [
+        {
+          character: "Bender",
+          quote: "I'm a fraud. A poor, lazy, sexy fraud.",
+          image:
+            "https://res.cloudinary.com/dzxqhkyqd/image/fetch/c_scale,w_500/https://res.cloudinary.com/dzxqhkyqd/image/upload/v1552429540/bender.png",
+        },
+      ],
     });
+
+    const output = await fetchQuotes();
+    expect(output).toEqual([
+      {
+        character: "Bender",
+        quote: "I'm a fraud. A poor, lazy, sexy fraud.",
+        image:
+          "https://res.cloudinary.com/dzxqhkyqd/image/fetch/c_scale,w_500/https://res.cloudinary.com/dzxqhkyqd/image/upload/v1552429540/bender.png",
+      },
+    ]);
   });
 });
