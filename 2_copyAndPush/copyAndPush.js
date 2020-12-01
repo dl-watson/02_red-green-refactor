f2ad35a5edda29aca7d870e258a02772da00e1cb
dl-watson <dlarkinwatson@gmail.com>
2020-12-01 14:03:17 -0800

    using push to mutate and return original array, test passes

--- 2_copyAndPush/copyAndPush.js
+++ 2_copyAndPush/copyAndPush.js
@@ -1,3 +1,11 @@
-const copyAndPush = () => {};
+const numbers = [1, 2, 3];
+const val = 4;
+
+const copyAndPush = (arr, val) => {
+  arr.push(val);
+  return arr;
+};
 
 module.exports = { copyAndPush };
+
+console.log(copyAndPush(numbers, val));
