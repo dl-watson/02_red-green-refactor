431e9d89a4cecc961a6020616d8465c684cc96f5
dl-watson <dlarkinwatson@gmail.com>
2020-12-01 13:48:45 -0800

    using bracket notation to access the object name property, test passes

--- 1_getName/getName.js
+++ 1_getName/getName.js
@@ -4,8 +4,10 @@ const spot = { name: "spot", age: 5, weight: "20 lbs" };
 const character = { _id: "5cf5679a915ecad153ab68c9", name: "Aang" };
 // getName(character); // returns "Aang"
 
-const getName = () => {
-  return "spot";
+const getName = (obj) => {
+  return obj["name"];
 };
 
 module.exports = { getName };
+
+console.log(getName(spot));
--- 1_getName/getName.test.js
+++ 1_getName/getName.test.js
@@ -2,8 +2,20 @@ const { getName } = require("./getName");
 
 describe("getName", () => {
   it("returns the name property of the passed object", () => {
-    const name = "spot";
+    const spot = { name: "spot", age: 5, weight: "20 lbs" };
+    const name = getName(spot);
 
     expect(name).toEqual("spot");
   });
 });
+
+/* 
+create your function, use bracket notation to get the objects name property
+pass tests and commit
+refactor, use dot notation to get the objects name
+tests stay green, commit
+refactor, destructure to get the objects name
+tests stay green, commit
+refactor, destructure function parameters and return name
+tests stay green, commit
+*/
