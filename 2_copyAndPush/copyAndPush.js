c7935c916eae80de05505b70260c780da8e05f45
dl-watson <dlarkinwatson@gmail.com>
2020-12-01 14:08:24 -0800

    proving to myself that my prior solutions worked

--- 2_copyAndPush/copyAndPush.js
+++ 2_copyAndPush/copyAndPush.js
@@ -2,8 +2,8 @@ const numbers = [1, 2, 3];
 const val = 4;
 
 const copyAndPush = (arr, val) => {
-  let newarr = [];
-  newarr = [...arr, val];
+  let newarr = [...arr];
+  newarr.push(val);
   return newarr;
 };
 
