be4ccf74d63aea7f8dc4be6fb4fee93f242cbe34
dl-watson <dlarkinwatson@gmail.com>
2020-12-01 14:07:31 -0800

    realizing that i did the dan method this entire time

--- 2_copyAndPush/copyAndPush.test.js
+++ 2_copyAndPush/copyAndPush.test.js
@@ -2,13 +2,17 @@ const { copyAndPush } = require("./copyAndPush");
 
 describe("copyAndPush", () => {
   it("given an array and a new item, returns the original array with the passed number appended", () => {
-    const numbers = [1, 2, 3, 4];
+    const array = [1, 2, 3];
+    const value = 4;
+    const numbers = copyAndPush(array, value);
 
     expect(numbers).toEqual([1, 2, 3, 4]);
   });
 });
 
 /*
+create your function, use push to add item to array and return array
+green, commit
 oops we were supposed to return a copy
 add another test that makes sure that the original array is unchanged
 update code, use spread to copy and append
