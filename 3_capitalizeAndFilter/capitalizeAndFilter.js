5d34f90d69d240564263726490ac55a5ccf4ad4b
dl-watson <dlarkinwatson@gmail.com>
2020-12-01 14:23:46 -0800

    an extraneous line

--- 3_capitalizeAndFilter/capitalizeAndFilter.js
+++ 3_capitalizeAndFilter/capitalizeAndFilter.js
@@ -1,10 +1,9 @@
 const input = ["spot", "rover", "bingo", "fred"];
 
 const capitalizeAndFilter = (input) => {
-  const inputCopy = [...input];
   let output = [];
   for (let i = 0; i < input.length; i++) {
-    const word = inputCopy[i];
+    const word = input[i];
     if (word.charAt(0) !== "f") {
       output.push(word.toUpperCase());
     }
