915b99984b040919ca5cc337db0551eafb8e44de
dl-watson <dlarkinwatson@gmail.com>
2020-12-01 14:21:30 -0800

    using a for loop to capitalize and filter, test passes

--- 3_capitalizeAndFilter/capitalizeAndFilter.js
+++ 3_capitalizeAndFilter/capitalizeAndFilter.js
@@ -1,3 +1,17 @@
-const capitalizeAndFilter = () => {};
+const input = ["spot", "rover", "bingo", "fred"];
+
+const capitalizeAndFilter = (input) => {
+  const inputCopy = [...input];
+  let output = [];
+  for (let i = 0; i < input.length; i++) {
+    const word = inputCopy[i];
+    if (word.charAt(0) !== "f") {
+      output.push(word.toUpperCase());
+    }
+  }
+  return output;
+};
 
 module.exports = { capitalizeAndFilter };
+
+console.log(capitalizeAndFilter(input));
--- 3_capitalizeAndFilter/capitalizeAndFilter.test.js
+++ 3_capitalizeAndFilter/capitalizeAndFilter.test.js
@@ -3,8 +3,15 @@ const { capitalizeAndFilter } = require("./capitalizeAndFilter");
 describe("capitalizeAndFilter", () => {
   it("takes in an array of strings and returns an array of all capitalized strings, words beginning with F filtered out", () => {
     const input = ["spot", "rover", "bingo", "fred"];
-    const output = ["SPOT", "ROVER", "BINGO"];
+    const output = capitalizeAndFilter(input);
 
     expect(output).toEqual(["SPOT", "ROVER", "BINGO"]);
   });
 });
+
+/*
+Write a test
+create a function, use a for loop to get test to pass
+green, commit
+refactor, use array methods to instead of a for loop
+*/
